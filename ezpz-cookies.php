2413e437af9a57de2f786b08b8de362e713d2bee
Matt Weet <hello@mattweet.com>
2021-06-05 08:52:57 +0100

    Initial Commit

---
+++ ezpz-cookies.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name:  EZPZ Cookie Bar
+ * Plugin URI:   https://github.com/epls-design/ezpz-cookies/
+ * Description:  Simple GDPR compliant cookie bar for Wordpress. Allows the website administrator to add analytics code to the header, footer and body via the dashboard. These tracking codes will only run on user acceptance of the cookie policy.
+ * Version:      1.0.0
+ * Author:       EPLS Design
+ * Author URI:   https://epls.design
+ * License:      GPL-2.0+
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:  ezpz-cookies
+ */
+
+// If this file is called directly, abort.
+if ( ! defined('ABSPATH')) {
+	exit;
+}
